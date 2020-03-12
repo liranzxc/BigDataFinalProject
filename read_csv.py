7fa6fae599bfec3b142b58e28a22aa150d173c82
danrol <danrol92@gmail.com>
2020-03-12 22:54:53 +0200

    read_csv  improve

--- read_csv.py
+++ read_csv.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import json
-from baseClasses.consumer import Consumer
 from baseClasses.producer import Producer
 
-# artist_header_label = "artist"
-# song_name_header_label = "song_name"
-# lyrics_label = "text"
 BOOTSTRAP_SERVER = "172.25.0.12:9092"
 TOPIC_NAME = "song"
 
@@ -17,16 +13,12 @@ def read_csv(filepath="datasets/songdata.csv"):
     return df_songs, headers, songs_json
 
 
-# def get_json_songs():
-#     global json_songs
-#     with open('lyrics.json', 'r') as f:
-#         data = f.read()
-#         json_songs = json.loads(data)
-#     pprint.pprint(json_songs)
-
-
-if __name__ == "__main__":
+def main():
     df_songs, headers, songs_json = read_csv()
     producer = Producer(BOOTSTRAP_SERVER, TOPIC_NAME)
     future = producer.send(songs_json[0])
     result = future.get(timeout=0.5)
+
+
+if __name__ == "__main__":
+    main()
