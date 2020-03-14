0bd647d4286958d2f385e05e301e68286bc08134
LiranNachman <lirannh@aidock.net>
2020-03-14 13:15:17 +0200

    add receive worker

--- baseClasses/consumer.py
+++ baseClasses/consumer.py
@@ -3,15 +3,17 @@ import json
 
 
 class Consumer:
-    def __init__(self, kafkaServer, topicReceive):
+    def __init__(self, kafka_server, topic_receive):
         self.consumer = KafkaConsumer(
-            topicReceive,
-            bootstrap_servers=[kafkaServer],
+            topic_receive,
+            auto_offset_reset='earliest',
+            bootstrap_servers=[kafka_server],
+            group_id=None,
             api_version=(0, 11, 5))
-        self.topicReceive = topicReceive
+        self.topic_receive = topic_receive
 
     def startReceive(self, methodMessages):
-        print("start receive from topic '{}'".format(self.topicReceive))
+        print("start receive from topic '{}'".format(self.topic_receive))
         for message in self.consumer:
             # message value and key are raw bytes -- decode if necessary!
             # e.g., for unicode: `message.value.decode('utf-8')`
--- consumer_main.py
+++ consumer_main.py
@@ -3,16 +3,13 @@ from services.config_service import ConfigService
 
 
 def doWork(data):
-    print(data)
-
+    pass
 
 if __name__ == "__main__":
     configService = ConfigService()
     config = configService.getConfig()
-
     BOOTSTRAP_SERVER = config["KAFKA_HOST"] + ":" + config["KAFKA_PORT"]
     worker = Consumer(BOOTSTRAP_SERVER, config["UPLOAD_TOPIC"])
-
     worker.startReceive(doWork)
 
     # lyrics = "Black is the night, metal we fight Power amps set to explode. Energy screams, magic and dreams Satan records the first note. We chime the bell, chaos and hell Metal for maniacs pure. Faster than steel, fortune on wheels Brain haemorrhage is the cure."
