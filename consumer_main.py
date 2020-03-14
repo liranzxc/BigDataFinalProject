f9b33b87531eb680b1f980d40d44db1bc0a8f054
LiranNachman <lirannh@aidock.net>
2020-03-14 13:21:47 +0200

    add receive worker

--- consumer_main.py
+++ consumer_main.py
@@ -1,16 +1,27 @@
 from baseClasses.consumer import Consumer
 from services.config_service import ConfigService
-
+from services.song_analyzer_service import SongAnalyzerService
+from pyspark import SparkContext,SparkConf
 
 def doWork(data):
-    pass
+    song = data[0]
+
+    # analyzer song and get result
+
+    # save result on db mongo
+
 
 if __name__ == "__main__":
-    configService = ConfigService()
-    config = configService.getConfig()
-    BOOTSTRAP_SERVER = config["KAFKA_HOST"] + ":" + config["KAFKA_PORT"]
-    worker = Consumer(BOOTSTRAP_SERVER, config["UPLOAD_TOPIC"])
-    worker.startReceive(doWork)
+
+    sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+    song_analyzer = SongAnalyzerService(sc)
+    print("here")
+
+    # configService = ConfigService()
+    # config = configService.getConfig()
+    # BOOTSTRAP_SERVER = config["KAFKA_HOST"] + ":" + config["KAFKA_PORT"]
+    # worker = Consumer(BOOTSTRAP_SERVER, config["UPLOAD_TOPIC"])
+    # worker.startReceive(doWork)
 
     # lyrics = "Black is the night, metal we fight Power amps set to explode. Energy screams, magic and dreams Satan records the first note. We chime the bell, chaos and hell Metal for maniacs pure. Faster than steel, fortune on wheels Brain haemorrhage is the cure."
     # analyzer = SongAnalyzer(Song("Venom", "Black Metal", lyrics))
--- services/song_analyzer_service.py
+++ services/song_analyzer_service.py
@@ -1,25 +1,27 @@
+# SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+from models.song import Song
+from models.song_profile import SongProfile
 
-#SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+
 class SongAnalyzerService:
-    def __init__(self,sc):
+    def __init__(self, sc):
         self.sc = sc
 
     # counts amount of words in the string
-    def _count_words(self,rdd_lyrics_song):
+    def _count_words(self, rdd_lyrics_song):
         return rdd_lyrics_song.count()
 
-    def _create_histogram(self,rdd_lyrics_song):
+    def _create_histogram(self, rdd_lyrics_song):
         return rdd_lyrics_song.map(lambda word: (word.lower(), 1)).reduceByKey(lambda a, b: a + b).collectAsMap()
 
-    def _init_lyrics_rdd(self,song:Song):
+    def _init_lyrics_rdd(self, song: Song):
         return self.sc.parallelize([song.lyrics]).flatMap(lambda line: line.split())
 
-    def analyze(self,song:Song) :
+    def analyze(self, song: Song):
         rdd_lyrics_song = self._init_lyrics_rdd(song)
         histogram = self._create_histogram(rdd_lyrics_song)
         word_count = self._count_words(rdd_lyrics_song)
-        return  SongProfile(song,word_count,histogram,None)# emo none
-
+        return SongProfile(song, word_count, histogram, None)  # emo none
 
     #
     # def __str__(self):
