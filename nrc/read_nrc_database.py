150070d443fc4317b5edf7f561e74d847f91a2ac
eden <eg7eg7@gmail.com>
2020-03-16 02:28:49 +0200

    added emotions using NRC

--- nrc/read_nrc_database.py
+++ nrc/read_nrc_database.py
@@ -1,36 +1,33 @@
-import pandas as pd
-
-from services.spark_service import SparkService
 from services.config_service import ConfigService
-from pyspark import SparkConf
-from pyspark import SparkContext
 from pyspark.shell import sqlContext
 from pyspark.sql import types
 from pyspark.sql.functions import col
+from nltk.stem import WordNetLemmatizer
 
 
 class NRC:
-    def __init__(self, filePath="./datasets/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt"):
-        configService = ConfigService()
-        self.config = configService.getConfig()
-        self.sc = SparkService(self.config).get_spark_context()
-        print("connected to spark")
+    def __init__(self, file_path="datasets/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt"):
+        config_service = ConfigService()
+        self.config = config_service.getConfig()
+        print("nrc connected to spark")
         df = sqlContext \
             .read.format("com.databricks.spark.csv") \
-            .schema(self.schemaFile()).option("delimiter", "\t").load(filePath)
-        cleanDF = df.na.drop()
-        self.emolex_df = cleanDF
+            .schema(self.schema_file()).option("delimiter", "\t").load(file_path)
+        clean_df = df.na.drop()
+        self.emotion_lex_df = clean_df
+        self.lemmatizer = WordNetLemmatizer()
 
-    def findArrayOfFeelingByWord(self, word):
-        filterDF = self.emolex_df \
+    def get_emotions_association(self, word):
+        word = self.lemmatizer.lemmatize(word)
+        filter_df = self.emotion_lex_df \
             .filter((col("word") == word) & (col("association") == 1)) \
             .select("emotion") \
             .collect()
-        new_list = [row.emotion for row in filterDF]
+        new_list = [row.emotion for row in filter_df]
         return new_list
 
     @staticmethod
-    def schemaFile():
+    def schema_file():
         schema = types.StructType(
             [
                 types.StructField('word', types.StringType()),
@@ -39,10 +36,9 @@ class NRC:
             ])
         return schema
 
-
 ## test
-words = ["love", "happy", "kill"]
-nrc = NRC()
-for w in words:
-    print(nrc.findArrayOfFeelingByWord(w))
-    print(" " * 45)
+# words = ["love", "happy", "kill"]
+# nrc = NRC()
+# for w in words:
+#    print(nrc.findArrayOfFeelingByWord(w))
+#    print(" " * 45)
--- utils/utilities.py
+++ utils/utilities.py
@@ -4,9 +4,8 @@ class Utilities:
         cleaned = ""
         numbers = '0123456789'
         not_allowed = '!,.?":;@#$%^&*()=+-\\'
-        if allow_numbers:
+        if not allow_numbers:
             not_allowed += numbers
-
         for char in word:
             if char in not_allowed:
                 char = ""
@@ -14,10 +13,10 @@ class Utilities:
         return cleaned
 
     @staticmethod
-    def clean_sentence(sentence, allow_numbers=False):
+    def clean_sentence(sentence):
         words1 = []
         words2 = sentence.split()
         for word in words2:
-            word = Utilities.clean_word(word, allow_numbers)
+            word = Utilities.clean_word(word)
             words1.append(word)
         return words1
