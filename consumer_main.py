11ae978830cb43d5f539b1197fe0748cdb2c21d8
LiranNachman <lirannh@aidock.net>
2020-03-25 16:42:36 +0200

    parallel jobs

--- consumer_main.py
+++ consumer_main.py
@@ -1,3 +1,4 @@
+import concurrent
 from concurrent.futures import ThreadPoolExecutor
 
 from baseClasses.consumer import Consumer
@@ -10,28 +11,33 @@ import nltk
 from models.song_profile import Song
 import sys
 import os
+import threading
 
 
-def do_work(data, extra):
+def do_work(data, extraData=None):
     song_profiles = []
     print("Consumer received a new batch")
     for song_json in data:
         # analyzer song and get result
         print("Consumer working on song")
         song = Song.from_json_to_song(song_json)
-        analyze = extra["song_analyzer"].analyze(song)
+        analyze = extraData["song_analyzer"].analyze(song)
+        print(extraData["consumerNumber"])
         print(analyze)
         song_profiles.append(analyze)
 
     # save song_profiles on db mongo
     songs_profile_jsons_array = list(map(lambda sp: sp.to_mongodb_document_format(), song_profiles))
-    x = extra["mongodb_service"].upload_song_profiles(songs_profile_jsons_array)
+    x = extraData["mongodb_service"].upload_song_profiles(songs_profile_jsons_array)
 
     # print list of the _id values of the inserted documents:
     print(x.inserted_ids)
 
 
-def customer_main_thread(config, mongodb_service):
+def customer_main_thread(consumerNumber):
+    config = ConfigService()
+    mongodb_service = MongoDbService(config)
+
     MAX_MEMORY = "5g"
     sc = SparkContext \
         .getOrCreate(SparkConf().set("spark.executor.memory", MAX_MEMORY)
@@ -46,17 +52,16 @@ def customer_main_thread(config, mongodb_service):
     # num_emotions = config.number_emotions
     BOOTSTRAP_SERVER = config.kafka_server_address
     worker = Consumer(BOOTSTRAP_SERVER, config.kafka_upload_topic)
-    worker.start_receive(do_work, extra={"song_analyzer": song_analyzer, "mongodb_service": mongodb_service})
+    worker.start_receive(do_work, extraData={"song_analyzer": song_analyzer,
+                                             "mongodb_service": mongodb_service,
+                                             "consumerNumber" : consumerNumber})
 
 
 if __name__ == "__main__":
     print('downloading wordnet...')
     nltk.download('wordnet')
     print('finish download wordnet.')
-    _config = ConfigService()
-    _mongodb_service = MongoDbService(_config)
-    customer_main_thread(_config, _mongodb_service)
-
-    executor = ThreadPoolExecutor(max_workers=10)
-    a = executor.submit(customer_main_thread, _config, _mongodb_service)
-    a.result(timeout=60*60*3600)
+    NUMBER_OF_CONSUMER = [0,1,2]
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number in executor.map(customer_main_thread, NUMBER_OF_CONSUMER):
+            print("consumer % finish".format(number))
