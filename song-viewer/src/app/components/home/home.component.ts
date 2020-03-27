2690c72b7559a5be5d7aa995318d8a977fd856bf
eg7eg7 <eg7eg7@gmail.com>
2020-03-27 23:19:41 +0300

    songs showing

--- song-viewer/src/app/components/home/home.component.ts
+++ song-viewer/src/app/components/home/home.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['artist', 'name', 'emotion', 'size'];
+  dataSource: MatTableDataSource<SongProfile>;
+  expandedElement: SongProfile | null;
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor() { 
-          // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+  constructor() {
+    // Create 100 users
+    const songProfiles = Array.from({ length: 2000 }, (_, k) => createNewSongProfile(k + 1));
 
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource(songProfiles);
 
   }
 
@@ -37,31 +46,50 @@ export class HomeComponent implements OnInit {
     }
   }
 }
-export interface UserData {
-  id: string;
+export interface SongProfile {
+  artist: string;
   name: string;
-  progress: string;
-  color: string;
+  lyrics: string;
+
+  size: number;
+  emotion: string;
+  histogram: object;
 }
 /** Constants used to fill up our data base. */
-const COLORS: string[] = [
+const SONG_NAMES: string[] = [
   'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
   'aqua', 'blue', 'navy', 'black', 'gray'
 ];
-const NAMES: string[] = [
+const ARTISTS: string[] = [
   'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
   'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
 ];
 
+const EMOTIONS: string[] = [
+  'Happy', 'Sad', 'Angry', 'Hateful', 'Joyful'
+];
+
 /** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+function createNewSongProfile(id: number): SongProfile {
+
+  const artist = ARTISTS[Math.round(Math.random() * (ARTISTS.length - 1))];
+
+  const emotion = EMOTIONS[Math.round(Math.random() * (EMOTIONS.length - 1))];
+
+  const song_name = SONG_NAMES[Math.round(Math.random() * (SONG_NAMES.length - 1))];
 
   return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    artist: artist,
+    name: song_name,
+    lyrics: 'djhf sdfisad sdlfksd',
+    size: Math.round(Math.random() * 1000),
+    emotion: emotion,
+    histogram: {
+      'a': Math.random(),
+      'b': Math.random(),
+      'c': Math.random()
+    }
   };
-}
+
+};
+
