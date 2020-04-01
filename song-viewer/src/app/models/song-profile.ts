e08de6e73a050ac448bd22d67f41a396b99bd17f
LiranNachman <lirannh@aidock.net>
2020-04-01 16:30:32 +0300

    1.4 date version 1 tested ,checked.

--- song-viewer/src/app/models/song-profile.ts
+++ song-viewer/src/app/models/song-profile.ts
@@ -1,15 +1,1 @@
-import { Song } from './song'
-export interface SongProfile {
-  song_name : string,
-  artist : string,
-  lyrics : string,
-  number_of_words :number;
-  histogram : {
-    barChartType?: any;
-    barChartLegend?: any;
-    barChartOptions?: any;
-    barChartLabels?: any;
-    barChartData?: any;
-  };
-  emotion :string;
-}
+export interface SongProfile {  song_name: string;  artist: string;  lyrics: string;  number_of_words: number;  histogram: {    barChartType?: any;    barChartLegend?: any;    barChartOptions?: any;    barChartLabels?: any;    barChartData?: any;  };  emotion: string;}
