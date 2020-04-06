1f7fa8467566bee52fd43ddc92f1cc0248db07e9
eg7eg7 <eg7eg7@gmail.com>
2020-04-06 18:27:42 +0300

    bat files

--- start_client.bat
+++ start_client.bat
@@ -1,2 +1,3 @@
 cd song-viewer
+call npm install
 ng serve --host 0.0.0.0
--- start_servers.bat
+++ start_servers.bat
@@ -1,2 +1,5 @@
 cd big-data-servers
+python -m pip install -r requirements-controller.txt
+docker-compose -f docker-compose-producer.yml build
+docker-compose -f docker-compose-consumer.yml build
 python controller_server.py
