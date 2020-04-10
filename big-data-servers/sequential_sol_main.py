9bc28c47983a34275e421ea182915123d4c87e73
Daniel Rolnik <danrol92@Gmail.com>
2020-04-10 21:18:24 +0300

    upd sequential

--- big-data-servers/sequential_sol_main.py
+++ big-data-servers/sequential_sol_main.py
@@ -2,12 +2,9 @@ from csv import reader
 from services.config_service import ConfigService
 import time
 
-
 artist_col_index = 0
 song_name_col_index = 1
 lyrics_col_index = 3
-
-
 
 def count_words(str):
     counts = dict()
@@ -21,10 +18,18 @@ def count_words(str):
 
     return counts
 
+def get_emotion(input):
+    for dict in input:
+        word = dict[0]
+        counter = dict[1]
+        print(f'word = {word}, counter = {counter}')
+
 if __name__ == "__main__":
     config = ConfigService()
     result_words_dict = dict()
     t = time.process_time()
+    emotions = {}
+
     with open(config.song_lyrics_csv, 'r') as read_obj:
         # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
@@ -36,11 +41,13 @@ if __name__ == "__main__":
             song_name = row[song_name_col_index]
             artist_name = row[artist_col_index]
             count_dict = count_words(row[lyrics_col_index])
+            sorted_count_dict = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
+            get_emotion(sorted_count_dict)
             if artist_name in result_words_dict:
-                result_words_dict[artist_name][song_name] = count_dict
+                result_words_dict[artist_name][song_name] = sorted_count_dict
             else:
                 result_words_dict[artist_name] = {}
-                result_words_dict[artist_name][song_name] = count_dict
+                result_words_dict[artist_name][song_name] = sorted_count_dict
 
     elapsed_time = time.process_time() - t
     print(f'Time elapsed since word count started: {elapsed_time}')
