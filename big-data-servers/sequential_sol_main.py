ef9ec7622fc68f46af64094b34416a2a79f0bf5b
eg7eg7 <eg7eg7@gmail.com>
2020-04-11 22:27:55 +0300

    using the models, cleaning sentences

--- big-data-servers/sequential_sol_main.py
+++ big-data-servers/sequential_sol_main.py
@@ -1,16 +1,18 @@
 from csv import reader
+
+from models.song import Song
+from models.song_profile import SongProfile
 from services.config_service import ConfigService
 import time
 from services.nrc_service import NRC
 
+ARTIST_COL = 0
+SONG_NAME_COL = 1
+LYRICS_COL = 3
 
-artist_col_index = 0
-song_name_col_index = 1
-lyrics_col_index = 3
 
-def count_words(str):
+def word_histogram(words):
     counts = dict()
-    words = str.split()
 
     for word in words:
         if word in counts:
@@ -19,42 +21,76 @@ def count_words(str):
             counts[word] = 1
     return counts
 
-def get_emotion(words_counts_tuples, nrc):
+
+def get_emotion(words_counts_tuples, _nrc):
     emotion = []
-    for tuple in words_counts_tuples:
-        word = tuple[0]
-        emotion.extend(nrc.get_emotions_association(word))
+    for _tuple in words_counts_tuples:
+        word = _tuple[0]
+        emotion.extend(_nrc.get_emotions_association(word))
         if len(emotion) is not 0:
             break
     return emotion
+
+
+def clean_word(word, allow_numbers=False):
+    cleaned = ""
+    numbers = '0123456789'
+    not_allowed = '!,.?":;@#$%^&*[]{}()=+-\\'
+    if not allow_numbers:
+        not_allowed += numbers
+    for char in word:
+        if char in not_allowed:
+            char = ""
+        cleaned += char
+    return cleaned
+
+
+def clean_sentence(sentence):
+    words1 = []
+    words2 = sentence.split()
+    for word in words2:
+        word = clean_word(word).lower()
+        words1.append(word)
+    return words1
+
+
+def clean_lyrics(lyrics_str):
+    lyrics_lst = []
+    lyrics_str = lyrics_str.split("\r\n")
+    for line in lyrics_str:
+        lyrics_lst.extend(clean_sentence(line))
+    return lyrics_lst
+
 
 if __name__ == "__main__":
     config = ConfigService()
     result_words_dict = dict()
     t = time.process_time()
-    emotions = {}
-
+    profiles = []
+    nrc = NRC(config)
     with open(config.song_lyrics_csv, 'r') as read_obj:
         # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
+
         # Iterate over each row in the csv using reader object
-        iterrows = iter(csv_reader) # skip header
-        next(iterrows)
-        for row in iterrows:
-            # row variable is a list that represents a row in csv
-            song_name = row[song_name_col_index]
-            artist_name = row[artist_col_index]
-            count_dict = count_words(row[lyrics_col_index])
-            sorted_count_dict = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
-            song_emotions = get_emotion(sorted_count_dict, NRC(config))
-            if artist_name in result_words_dict:
-                result_words_dict[artist_name][song_name] = sorted_count_dict
-            else:
-                result_words_dict[artist_name] = {}
-                emotions[artist_name] = {}
-                result_words_dict[artist_name][song_name] = sorted_count_dict
-            emotions[artist_name][song_name] = song_emotions
-            print("iteration ended")
+        iterrows = iter(csv_reader)
+        next(iterrows)  # skip header
+
+        for index, row in enumerate(iterrows):
+            # row represents a song entry in csv
+            song_name = row[SONG_NAME_COL]
+            artist_name = row[ARTIST_COL]
+            lyrics = row[LYRICS_COL]
+            song = Song(artist=artist_name, name=song_name, lyrics=lyrics)
+            lyrics = clean_lyrics(lyrics)
+            count = len(lyrics)
+            histogram = word_histogram(lyrics)
+            sorted_histogram = sorted(histogram.items(), key=lambda x: x[1], reverse=True)
+            song_emotions = get_emotion(sorted_histogram, nrc)
+            profile = SongProfile(song=song, number_of_words=count, histogram=histogram, emotion=song_emotions)
+            profiles.append(profile)
+            print("song {0} analyzed".format(index))
+            print(profile)
 
     elapsed_time = time.process_time() - t
-    print(f'Time elapsed since word count started: {elapsed_time}')
+    print(f'Time elapsed since song analysis started: {elapsed_time}')
