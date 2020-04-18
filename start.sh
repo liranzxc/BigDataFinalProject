ebdd681bf6a6910824b871bd63790c49b7c276b9
eg7eg7 <eg7eg7@gmail.com>
2020-04-18 22:17:40 +0300

    remove

--- start.sh
+++
@@ -1,6 +0,0 @@
-cd song-viewer
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
-npm install
-ng serve --host 0.0.0.0
--- start_server.sh
+++
@@ -1,5 +0,0 @@
-cd big-data-servers
-pip install -r requirements-controller.txt
-docker-compose -f docker-compose-producer.yml build
-docker-compose -f docker-compose-consumer.yml build
-python controller_server.py
